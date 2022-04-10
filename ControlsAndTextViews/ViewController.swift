94f1d0c504925a17af78525f56e8e6811c5fc1d1
Camilo González <cgonzalezm1234@gmail.com>
2022-04-10 08:28:54 -0400

    Initial Commit

---
+++ ControlsAndTextViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ControlsAndTextViews
+//
+//  Created by Camilo Gonzalez on 10-04-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
